47d6f5cd34ea1868c05c4eee0709ba90ac5bfc2f
Florian Schlachter <flori@n-schlachter.de>
2014-07-01 16:22:17 +0200

    documentation for Must, FromString, FromFile.

--- pongo2.go
+++ pongo2.go
@@ -4,6 +4,9 @@ import (
 	"io/ioutil"
 )
 
+// Helper function which panics, if a Template couldn't
+// successfully parsed. This is how you would use it:
+//     var baseTemplate = pongo2.Must(pongo2.FromFile("templates/base.html"))
 func Must(tpl *Template, err error) *Template {
 	if err != nil {
 		panic(err)
@@ -11,11 +14,15 @@ func Must(tpl *Template, err error) *Template {
 	return tpl
 }
 
+// Loads  a template from string and returns a Template instance.
 func FromString(tpl string) (*Template, error) {
 	t, err := newTemplateString(tpl)
 	return t, err
 }
 
+// Loads  a template from a filename and returns a Template instance.
+// The filename must either be relative to the application's directory
+// or be an absolute path.
 func FromFile(filename string) (*Template, error) {
 	buf, err := ioutil.ReadFile(filename)
 	if err != nil {
