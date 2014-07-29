fc7be0cf4e52113d24335bcac6690a1500a0e54a
Florian Schlachter <flori@n-schlachter.de>
2014-07-29 18:44:15 +0200

    cycle-tag improved.

--- tags_cycle.go
+++ tags_cycle.go
@@ -4,11 +4,21 @@ import (
 	"bytes"
 )
 
+type tagCycleValue struct {
+	node  *tagCycleNode
+	value *Value
+}
+
 type tagCycleNode struct {
 	position *Token
 	args     []INodeEvaluator
 	idx      int
 	as_name  string
+	silent   bool
+}
+
+func (cv *tagCycleValue) String() string {
+	return cv.value.String()
 }
 
 func (node *tagCycleNode) Execute(ctx *ExecutionContext, buffer *bytes.Buffer) error {
@@ -20,15 +30,44 @@ func (node *tagCycleNode) Execute(ctx *ExecutionContext, buffer *bytes.Buffer) e
 		return err
 	}
 
-	if node.as_name != "" {
-		ctx.Private[node.as_name] = val
+	if t, ok := val.Interface().(*tagCycleValue); ok {
+		// {% cycle "test1" "test2"
+		// {% cycle cycleitem %}
+
+		// Update the cycle value with next value
+		item := t.node.args[t.node.idx%len(t.node.args)]
+		t.node.idx++
+
+		val, err := item.Evaluate(ctx)
+		if err != nil {
+			return err
+		}
+
+		t.value = val
+
+		if !t.node.silent {
+			buffer.WriteString(val.String())
+		}
 	} else {
-		buffer.WriteString(val.String())
+		// Regular call
+
+		cycle_value := &tagCycleValue{
+			node:  node,
+			value: val,
+		}
+
+		if node.as_name != "" {
+			ctx.Private[node.as_name] = cycle_value
+		}
+		if !node.silent {
+			buffer.WriteString(val.String())
+		}
 	}
 
 	return nil
 }
 
+// HINT: We're not supporting the old comma-seperated list of expresions argument-style
 func tagCycleParser(doc *Parser, start *Token, arguments *Parser) (INodeTag, error) {
 	cycle_node := &tagCycleNode{
 		position: start,
@@ -49,6 +88,10 @@ func tagCycleParser(doc *Parser, start *Token, arguments *Parser) (INodeTag, err
 				return nil, arguments.Error("Name (identifier) expected after 'as'.", nil)
 			}
 			cycle_node.as_name = name_token.Val
+
+			if arguments.MatchOne(TokenIdentifier, "silent") != nil {
+				cycle_node.silent = true
+			}
 
 			// Now we're finished
 			break
