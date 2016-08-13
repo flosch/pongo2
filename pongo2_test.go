ea5e8b93d2bbd9692cf5757510e45dab2a99fb45
Andrew Stone <a@stoney.io>
2016-08-13 12:54:17 -0400

    Revert "Merge pull request #137 from flosch/revert-84-implicit-exec"
    This reverts commit f3588f81319388d4c8165fcd213498afe3c69b4d, reversing
    changes made to f5c81908d63555d4762ae53df304fb54c59f3160.

--- pongo2_test.go
+++ pongo2_test.go
@@ -68,3 +68,38 @@ func (s *TestSuite) TestMisc(c *C) {
 		}
 	}, PanicMatches, `\[Error \(where: applyfilter\)\] Filter with name 'doesnotexist' not found.`)
 }
+
+func (s *TestSuite) TestImplicitExecCtx(c *C) {
+	tpl, err := FromString("{{ ImplicitExec }}")
+	if err != nil {
+		c.Fatalf("Error in FromString: %v", err)
+	}
+
+	val := "a stringy thing"
+
+	res, err := tpl.Execute(Context{
+		"Value": val,
+		"ImplicitExec": func(ctx *ExecutionContext) string {
+			return ctx.Public["Value"].(string)
+		},
+	})
+
+	if err != nil {
+		c.Fatalf("Error executing template: %v", err)
+	}
+
+	c.Check(res, Equals, val)
+
+	// The implicit ctx should not be persisted from call-to-call
+	res, err = tpl.Execute(Context{
+		"ImplicitExec": func() string {
+			return val
+		},
+	})
+
+	if err != nil {
+		c.Fatalf("Error executing template: %v", err)
+	}
+
+	c.Check(res, Equals, val)
+}
