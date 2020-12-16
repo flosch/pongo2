6b600819ffbf4a36a02b27f81065fdf1fc03bfa9
Florian Schlachter <flori@n-schlachter.de>
2020-12-16 17:59:27 +0100

    bump version 4.0.2

--- pongo2.go
+++ pongo2.go
@@ -1,7 +1,7 @@
 package pongo2
 
 // Version string
-const Version = "4.0.1"
+const Version = "4.0.2"
 
 // Must panics, if a Template couldn't successfully parsed. This is how you
 // would use it:
